2c3898a45e6859c983c5c2348e65893efc7c75e5
AI-yna <adela.iliescu@yjb.gov.uk>
2023-05-23 13:26:42 +0100

    changed folder structure

--- hudau/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(name = "hudau",
-      version = 0.1,
-      description="Package containing functions commonly used by the YJB Statistics and Analysis Team",
-      author="AI_ynnna",
-      author_email="adela.iliescu@yjb.gov.uk",
-      packages=find_packages()
-      )

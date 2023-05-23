517484c01a08d9b752ac98ff13714363b0a8681f
AI-yna <adela.iliescu@yjb.gov.uk>
2023-05-23 16:01:30 +0100

    fixed path for check_non_matching_names

--- hudau/datacleaning.py
+++ hudau/datacleaning.py
@@ -51,7 +51,7 @@ def check_non_matching_names(df_cms, yjs_col):
     A set with the non-matching names if there are or a message confirming there are no non-matches. 
     """   
 
-    yot_names = pd.read_csv('data/raw/YJS_names_standardised.csv')
+    yot_names = pd.read_csv('data/YJS_names_standardised.csv')
     values_yjs_standardised = list(yot_names['yjs_name'].unique())
     values_yots_cms = list(df_cms[yjs_col].unique())
     in_cms_not_yjs = set(values_yots_cms) -set(values_yjs_standardised)
